3d383e9c6bbadd36889433cfcfc7bf7de6f2708c
JamesCSmithPDX <james.c.smithpdx@gmail.com>
2016-04-01 12:18:24 -0700

    Finalized domAppend function

--- main.js
+++ main.js
@@ -24,32 +24,25 @@ storesArray.push(new CookieStore("Sellwood", 20, 48, 3.3));
 storesArray.push(new CookieStore("Pearl District", 3, 24, 2.6));
 
 var storeHours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-// Create table header for store hours
-function domAppend(element, text, parent) {
+
+// function to build table
+function domAppend(element, text, parent, index) {
   console.log(element, text, parent);
   var elementName = document.createElement(element);
   var textNode = document.createTextNode(text);
   elementName.appendChild(textNode);
-  var elementPosition = document.getElementsByTagName(parent)[0];
+  var elementPosition = document.getElementsByTagName(parent)[index];
   elementPosition.appendChild(elementName);
   }
 
+//Create table header
 function tableHeader() {
   for (var tableIndex = 0; tableIndex < storeHours.length; tableIndex++) {
-      var headerText = storeHours[tableIndex];
-      console.log(headerText);
-      domAppend("th", headerText, "tr");
+    console.log(storeHours[tableIndex]);
+    domAppend("th", storeHours[tableIndex], "tr", 0);
     };
-
-    domAppend("th", "Total", "tr");
-    // Add total header
-    // var tableTotal = document.createElement('th');
-    // var tableTotalText = document.createTextNode('Total');
-    // tableTotal.appendChild(tableTotalText);
-    // var totalPosition = document.getElementsByTagName('tr')[0];
-    // totalPosition.appendChild(tableTotal);
+    domAppend("th", "Total", "tr", 0);
   }
-
   tableHeader()
 
 // Create store rows, get store names as row headers, populate with sales simulations
@@ -58,32 +51,17 @@ function tableHeader() {
 
     function cookiesHourlySim(store) {
       var cookiesTotal = 0; // sums hourly estimates for daily total
-      var newTableElement = document.createElement('tr');
-      var trPosition = document.getElementsByTagName('tbody')[0]; // find location for tr
-      trPosition.appendChild(newTableElement); // post to page
-      // create tr for each store
-      var thStoreName = document.createElement('th');
-      var thNameText = document.createTextNode(store.location); // store name as text node
-      thStoreName.appendChild(thNameText); //append name to tr
-      var thStorePosition =
-      document.getElementsByTagName('tr')[storeIndex + 1];
-      thStorePosition.appendChild(thStoreName);
+      domAppend("tr","","tbody", 0); //create new row
+      domAppend("th", store.location, "tr", storeIndex + 1); //add store name
 
       for (var tableIndex = 0; tableIndex < storeHours.length; tableIndex++) {
         var cookiePurchase = store.custHourlySim();// estimates sales for hour
         cookiesTotal += cookiePurchase; // sums total sales
-        var newTableItem = document.createElement('td'); // create td for each hour
-        var tableItemText = document.createTextNode(cookiePurchase) // create text for hourly li
-        newTableItem.appendChild(tableItemText); // add text to td
-        var tdPosition = document.getElementsByTagName('tr')[storeIndex + 1]; // get location post to page
-        tdPosition.appendChild(newTableItem); // post element & text to page
+        domAppend("td", cookiePurchase, "tr", storeIndex + 1); //adds results to table
         store.cookieSimArray.push(cookiePurchase); // push estimate to store array
-    };
+      };
+
     // add total sales to bottom of store list
-      var newTotalItem = document.createElement('th');
-      var totalText = document.createTextNode(cookiesTotal);
-      newTotalItem.appendChild(totalText);
-      var tdTotalPosition = document.getElementsByTagName('tr')[storeIndex + 1];
-      tdTotalPosition.appendChild(newTotalItem);
+      domAppend("th", cookiesTotal, "tr", storeIndex + 1);
     }
   };
